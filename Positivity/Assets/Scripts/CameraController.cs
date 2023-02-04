28dd5ef022e1bcff21f366904152d26138147c35
JoshWilger <hap3guy@gmail.com>
2023-02-04 10:02:23 -0600

    Added camera effect

--- Positivity/Assets/Scripts/CameraController.cs
+++ Positivity/Assets/Scripts/CameraController.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private EdgeCollider2D terrain;
+
+    private float cameraWidth;
+
+
+    private void Start()
+    {
+        cameraWidth = Camera.main.orthographicSize * Screen.width / Screen.height + 0.1f;
+    }
 
     // Update is called once per frame
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, terrain.bounds.min.x + cameraWidth, terrain.bounds.max.x - cameraWidth),
+            Mathf.Clamp(player.position.y, terrain.bounds.min.y, terrain.bounds.max.y), transform.position.z);
     }
 }
