2198fe50183b5fb2bfb9dd101df7675725cff0b3
Kevin <kmoens@lakeco.com>
2023-02-04 16:30:45 -0600

    hold position of player and watering can

--- Positivity/Assets/Scripts/LevelSelectorInteraction.cs
+++ Positivity/Assets/Scripts/LevelSelectorInteraction.cs
@@ -6,15 +6,27 @@ using UnityEngine.SceneManagement;
 public class LevelSelectorInteraction : MonoBehaviour
 {
     //Property For The BlueHouse
-
     public Transform playerPosition;
+    public Transform canPosition;
     private const float detectionRadius = 0.00001f;
     public LayerMask detectionLayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Retrieve Player Position
+        string mainScenePlayerPosition = PlayerPrefs.GetString("MainScenePlayerPosition");
+        if (string.IsNullOrWhiteSpace(mainScenePlayerPosition) == false)
+        {
+            var posValues = mainScenePlayerPosition.Split(',');
+            playerPosition.position = new Vector3(float.Parse(posValues[0]), float.Parse(posValues[1]), float.Parse(posValues[2]));
+        }
+        string mainSceneCanPosition = PlayerPrefs.GetString("MainSceneCanPosition");
+        if (string.IsNullOrWhiteSpace(mainSceneCanPosition) == false)
+        {
+            var posValues = mainSceneCanPosition.Split(',');
+            canPosition.position = new Vector3(float.Parse(posValues[0]), float.Parse(posValues[1]), float.Parse(posValues[2]));
+        }
     }
 
     // Update is called once per frame
@@ -32,8 +44,10 @@ public class LevelSelectorInteraction : MonoBehaviour
         {
             return;
         }
-        
-         Debug.Log(item.name);
+
+        //Save Player Position
+        PlayerPrefs.SetString("MainScenePlayerPosition", $"{playerPosition.position.x},{playerPosition.position.y},{playerPosition.position.z}");
+        PlayerPrefs.SetString("MainSceneCanPosition", $"{canPosition.position.x},{canPosition.position.y},{canPosition.position.z}");
         switch (item.name)
         {
             case "BlueHouse":
@@ -43,6 +57,7 @@ public class LevelSelectorInteraction : MonoBehaviour
                 SceneManager.LoadScene("CarlyHouse");
                 break;
             case "OrangeHouse":
+
                 SceneManager.LoadScene("AllenHouse");
                 break;
             case "PurpleHouse":
--- Positivity/Assets/Scripts/StartScreen.cs
+++ Positivity/Assets/Scripts/StartScreen.cs
@@ -7,6 +7,12 @@ using UnityEngine.SceneManagement;
 public class StartScreen : MonoBehaviour
 {
     public Animator animator;
+    private void Start()
+    {
+        //Default Player Position for later
+        PlayerPrefs.SetString("MainScenePlayerPosition", $"21.37,0.86,0");
+        PlayerPrefs.SetString("MainSceneCanPosition", $"2,1,0");
+    }
 
     public void PlayButton_Clicked()
     {
