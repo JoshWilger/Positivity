3b1440e97a94f751769d6f833016e2bd37921f5d
Kevin <kmoens@lakeco.com>
2023-02-04 09:42:07 -0600

    Changed more start up screen things

--- Positivity/Assets/Scripts/StartScreen.cs
+++ Positivity/Assets/Scripts/StartScreen.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScreen : MonoBehaviour
 {
+    public Animator animator;
 
     public void PlayButton_Clicked()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+    public void OnFadeComplete()
     {
         SceneManager.LoadScene("MainScene");
     }
