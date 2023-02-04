7771b932a58f6f255e81a33f4a4156cc2d05bb4d
Kevin <kmoens@lakeco.com>
2023-02-04 10:43:29 -0600

    Handle Selecting BlueHouse and going to Ground Area Scene

--- Positivity/Assets/Scripts/StartScreen.cs
+++ Positivity/Assets/Scripts/StartScreen.cs
@@ -14,6 +14,6 @@ public class StartScreen : MonoBehaviour
     }
     public void OnFadeComplete()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("LevelSelector");
     }
 }
