52726a6e05c65209d961b7a633df73d9934c9683
Cheezeh3d <newjacobzoerb@gmail.com>
2023-02-04 10:42:27 -0600

    Added Margret scene. Adjusted collision and camera physics

--- Positivity/Assets/Scripts/CameraController.cs
+++ Positivity/Assets/Scripts/CameraController.cs
@@ -8,17 +8,19 @@ public class CameraController : MonoBehaviour
     [SerializeField] private EdgeCollider2D terrain;
 
     private float cameraWidth;
+    private float cameraHeight;
 
 
     private void Start()
     {
         cameraWidth = Camera.main.orthographicSize * Screen.width / Screen.height + 0.1f;
+        cameraHeight = Camera.main.orthographicSize;
     }
 
     // Update is called once per frame
     private void Update()
     {
         transform.position = new Vector3(Mathf.Clamp(player.position.x, terrain.bounds.min.x + cameraWidth, terrain.bounds.max.x - cameraWidth),
-            Mathf.Clamp(player.position.y, terrain.bounds.min.y, terrain.bounds.max.y), transform.position.z);
+            Mathf.Clamp(player.position.y, terrain.bounds.min.y + cameraHeight, terrain.bounds.max.y - cameraHeight), transform.position.z);
     }
 }
