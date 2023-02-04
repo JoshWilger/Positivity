abc7ce9eb9ed4421e33d7b87c6154f4c4abbe5ce
JoshWilger <hap3guy@gmail.com>
2023-02-04 00:04:04 -0600

    Added watering can functionality

---
+++ Positivity/Assets/Scripts/ItemSpriteSwap.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpriteSwap : MonoBehaviour
+{
+    [SerializeField] private GameObject objective;
+    [SerializeField] private Sprite newSprite;
+
+    private SpriteRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == objective)
+        {
+            render.sprite = newSprite;
+        }
+    }
+}
---
+++ Positivity/Assets/Scripts/ItemStick.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStick : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] objectives;
+
+    private Vector3 offset = new();
+    private GameObject collidedObject = null;
+    private GameObject theOtherObject = null;
+    private Rigidbody2D rb;
+    private bool completed = false;
+    private int currentIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        collidedObject = theOtherObject; // do collision check, replace with collided object
+        if (collidedObject != null && !completed)
+        {
+            transform.position = collidedObject.transform.position + offset;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (completed)
+        {
+            return;
+        }
+        if (collision.gameObject == player && !completed)
+        {
+            theOtherObject = collision.gameObject;
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            offset = transform.position - theOtherObject.transform.position;
+        }
+        else if (collision.gameObject == objectives[currentIndex])
+        {
+            currentIndex++;
+            completed = currentIndex == objectives.Length;
+        }
+
+        if (completed)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            theOtherObject = collision.gameObject;
+            offset = transform.position - theOtherObject.transform.position;
+        }
+    }
+}
