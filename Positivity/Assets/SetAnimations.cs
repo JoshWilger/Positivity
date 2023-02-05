87c52ec540da2ef7547def0145e371fe50f155a9
Cheezeh3d <newjacobzoerb@gmail.com>
2023-02-05 11:10:07 -0600

    Added the rest of the music to be used throughout the game

--- Positivity/Assets/SetAnimations.cs
+++ Positivity/Assets/SetAnimations.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class SetAnimations : MonoBehaviour
 {
@@ -11,6 +13,7 @@ public class SetAnimations : MonoBehaviour
     public Animator anim3;
     public Animator anim4;
     public Animator anim5;
+    public PlayableDirector timeline;
 
 
     // Start is called before the first frame update
@@ -21,5 +24,13 @@ public class SetAnimations : MonoBehaviour
         anim3.SetTrigger("better");
         anim4.SetTrigger("better");
         anim5.SetTrigger("better");
+    }
+
+    void Update()
+    {
+        if(timeline.time >= 51)
+        {
+            SceneManager.LoadScene("StartScreen");
+        }
     }
 }
